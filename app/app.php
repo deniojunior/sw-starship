995a1cc6835499b439a45353706cae40fbe1b467
deniojunior <deniofriacamoreirajr@gmail.com>
2018-09-05 23:20:56 -0300

    Estrutuar projeto com o Laravel, #3
    Um novo projeto foi criado utilizando as informações padrão do Laravel

--- app/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-$includesPath = PATH_SEPARATOR . __DIR__;
-$includesPath .= PATH_SEPARATOR . __DIR__ . '/views';
-$includesPath .= PATH_SEPARATOR . get_include_path();
-
-
-set_include_path($includesPath);
